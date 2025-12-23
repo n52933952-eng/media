0ca12c8b0b7f1107b5110c1b10c280d5f77205dd
muahand-neyma <losylos084@gamil.com>
2025-12-23 14:09:02 -0800

    hjkk

--- frontent/src/Pages/MessagesPage.jsx
+++ frontent/src/Pages/MessagesPage.jsx
@@ -663,7 +663,7 @@ const MessagesPage = () => {
             {/* Message Input - Mobile optimized */}
             <Flex
               p={{ base: 2, md: 4 }}
-              pb={{ base: '24px', md: 4 }}
+              pb={{ base: '40px', md: 4 }}
               pt={{ base: 2, md: 4 }}
               borderTop="1px solid"
               borderColor={borderColor}
