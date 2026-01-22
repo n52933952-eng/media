521c06f69b6e8bfbd5196e0e41b45d964009d139
muahand-neyma <losylos084@gamil.com>
2026-01-22 02:38:48 -0800

    kfgjg

--- backend/utils/GenerateToken.js
+++ backend/utils/GenerateToken.js
@@ -3,9 +3,9 @@ import jwt from 'jsonwebtoken'
 
 const GenerateToken = (userId,res) => {
 
-    const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"5d"})
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"60d"})
 
-    res.cookie("jwt",token,{httpOnly:true,maxAge:15 * 24 * 60 * 1000,sameSite:"strict"})
+    res.cookie("jwt",token,{httpOnly:true,maxAge:60 * 24 * 60 * 60 * 1000,sameSite:"strict"})
 
 
     return token 
